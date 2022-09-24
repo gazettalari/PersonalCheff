3621e1dc35234b2ef792a9193fa8fc86c7414a43
Larissa Gazetta <108579379+gazettalari@users.noreply.github.com>
2022-09-24 11:41:19 -0300

    inserção do bootstrap e formatação da aplicação

--- PersonalCheff2/receitas/urls.py
+++ PersonalCheff2/receitas/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
         path('', views.index,  name='index'),
-        path('contato', views.contato, name)
+        path('contato', views.contato,  name='sucodelaranja'),
         path('sucodelaranja', views.sucodelaranja,  name='sucodelaranja'),
          path('sucodelimao', views.sucodelimao, name='sucodelimao'),
          path('bolodecenoura', views.bolodecenoura, name='bolodecenoura'),
